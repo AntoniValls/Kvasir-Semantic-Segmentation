e97621771114532d704561ffc65cacccff9eef27
Antoni Valls <101109878+AntoniValls@users.noreply.github.com>
2024-05-15 16:04:39 +0200

    Add files via upload

---
+++ src/loss.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import torch
+import torch.nn as nn
+
+class BCEDiceLoss(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self,input,target):
+        bce = F.binary_cross_entropy_with_logits(input,target)
+        smooth = 1e-5
+        input = torch.sigmoid(input)
+        num = target.size(0)
+        input = input.view(num,-1)
+        target = target.view(num,-1)
+        intersection = (input * target)
+        dice = (2. * intersection.sum(1) + smooth) / (input.sum(1) + target.sum(1) + smooth)
+        dice = 1 - dice.sum() / num
+        return (0.5 * bce + dice)
